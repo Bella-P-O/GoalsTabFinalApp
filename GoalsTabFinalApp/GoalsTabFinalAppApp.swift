3a67ea0af6c30bf814067adf83c6e8673333611c
Bella <bellaperezolle@gmail.com>
2023-08-16 09:45:35 -0400

    Initial Commit

---
+++ GoalsTabFinalApp/GoalsTabFinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsTabFinalAppApp.swift
+//  GoalsTabFinalApp
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GoalsTabFinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
